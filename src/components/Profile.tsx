79406b3b559dd1f4af9824bcb387b3f816aa6a39
Gislaine Costa Pereira <gislaine_costa@hotmail.com>
2021-02-24 00:02:00 -0300

    Criação do App, arquitetura de arquivos e primeiras estilizações

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile( ) {
+    return (
+        <div>
+            <img src="https://avatars.githubusercontent.com/u/62241763?s=460&u=9846af7d83a4e80ffe48b9264545f34a3e3b011a&v=4" alt="Perfil"/>
+            <div>
+                <strong>Gislaine Costa Pereira</strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/_document.tsx
@@ -0,0 +1,22 @@
+// Tudo que é estático
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
+                    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet" />
+                    <title>NLW4</title>
+                </Head>
+
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
